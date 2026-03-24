4d82a618d7281c1131cb6222a622bb7bf3d25ecb
= <eric.steinecke@gmailcom>
2026-03-24 13:55:47 -0400

    rn components => tensegrity, update build location

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { FormGenerator } from './router-outlets/form-generator/form-generator';
 
 export const routes: Routes = [
-    {
-        path: 'form', pathMatch: 'full', component: FormGenerator, title: 'Form Components'
-    },
-    {
-        path: '', pathMatch: 'full', redirectTo: 'form'
-    }
+  {
+    path: 'form',
+    pathMatch: 'full',
+    component: FormGenerator,
+    title: 'Form Components',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'form',
+  },
 ];
