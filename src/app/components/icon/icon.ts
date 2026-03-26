36ca9eb8da7e8f6c23d5951cb1c05783e47618fd
= <eric.steinecke@gmailcom>
2026-03-25 20:05:02 -0400

    link for icons multiple icons

--- src/app/components/icon/icon.ts
+++ src/app/components/icon/icon.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ContentChild, ElementRef, inject, input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { IconName, iconNames } from '../../models/icons.model';
 
 @Component({
@@ -9,6 +9,8 @@ import { IconName, iconNames } from '../../models/icons.model';
 })
 export class Icon implements AfterViewInit {
   constructor(public el: ElementRef) {}
+  stylesheetHref =
+    'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=';
 
   ngAfterViewInit(): void {
     const iconName = (this.el.nativeElement as HTMLElement).outerText as IconName;
@@ -16,8 +18,10 @@ export class Icon implements AfterViewInit {
       return console.warn(
         `[Icon => ngAfterViewInit()] ${iconName} is not a valid IconName. Please make sure that is is in the iconNames array.`,
       );
-    const stylesheetHref = this.getHref(iconName);
-    const stylesheet = document.querySelector(`[href="${stylesheetHref}"]`);
+    const stylesheetHref = this.stylesheetHref + iconName;
+    const stylesheet = document.querySelector(
+      `[href^="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"]`,
+    );
 
     if (!stylesheet) {
       const head = document.head;
@@ -25,9 +29,18 @@ export class Icon implements AfterViewInit {
       link.rel = 'stylesheet';
       link.href = stylesheetHref;
       head.appendChild(link);
+    } else {
+      const currentUrl = stylesheet.getAttribute('href');
+      const iconNames = currentUrl!.split('&icon_names=')!.slice(1);
+
+      if (iconNames.includes(iconName)) return;
+
+      iconNames.push(iconName);
+      iconNames.sort();
+
+      const newIconNamesString = iconNames.join(',');
+
+      stylesheet.setAttribute('href', this.stylesheetHref + newIconNamesString);
     }
   }
-
-  getHref = (iconName: IconName) =>
-    `https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=${iconName.toLowerCase()}`;
 }
